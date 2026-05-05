a107496ce2cd6222bdd930b60a990d4814732a99
Krzysztof Heller <krzysztof.heller@buddy.works>
2026-05-05 09:46:40 +0200

    update cert

--- certs/generate.sh
+++ certs/generate.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 openssl req -x509 -nodes -newkey rsa:2048 \
   -keyout mssql.key -out mssql.pem \
   -days 365 -subj "/CN=mssql-tls" \
-  -addext "subjectAltName=DNS:mssql-tls,DNS:localhost,IP:127.0.0.1"
+  -addext "subjectAltName=DNS:mssql-tls,DNS:localhost,IP:127.0.0.1,IP:192.168.10.226"
 
 chmod 644 mssql.pem mssql.key
 
